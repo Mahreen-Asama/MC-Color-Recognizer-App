b2e18fc21379e9c899ff1e5fabaf426c4be526eb
Mahreen-Asama <mahreenmehar202@gmail.com>
2022-05-19 23:44:54 +0500

    colors finalized

--- app/src/main/java/com/example/colorrecognizerforkids/ColorActivity.java
+++ app/src/main/java/com/example/colorrecognizerforkids/ColorActivity.java
@@ -44,8 +44,10 @@ public class ColorActivity extends AppCompatActivity {
                         colorName.setText("BLACK");
                         colorName.setTextColor(Color.BLACK);
                         star.setColorFilter(Color.BLACK);
-                        btnPrev.setBackgroundColor(Color.WHITE);
-                        btnNext.setBackgroundColor(Color.WHITE);
+                        btnPrev.setBackgroundColor(Color.BLACK);
+                        btnNext.setBackgroundColor(Color.BLACK);
+                        btnPrev.setColorFilter(Color.WHITE);
+                        btnNext.setColorFilter(Color.WHITE);
                         break;
                     case "BLACK":
                         colorName.setText("GRAY");
@@ -56,19 +58,31 @@ public class ColorActivity extends AppCompatActivity {
                         break;
                     case "GRAY":
                         colorName.setText("BROWN");
-                        colorName.setTextColor(Color.parseColor("#964B00"));
+                        colorName.setTextColor(Color.rgb(165,42,42));
+                        star.setColorFilter(Color.rgb(165,42,42));
+                        btnPrev.setBackgroundColor(Color.rgb(165,42,42));
+                        btnNext.setBackgroundColor(Color.rgb(165,42,42));
                         break;
                     case "BROWN":
                         colorName.setText("ORANGE");
-                        colorName.setTextColor(Color.parseColor("FFA500"));
+                        colorName.setTextColor(Color.rgb(255,165,0));
+                        star.setColorFilter(Color.rgb(255,165,0));
+                        btnPrev.setBackgroundColor(Color.rgb(255,165,0));
+                        btnNext.setBackgroundColor(Color.rgb(255,165,0));
                         break;
                     case "ORANGE":
                         colorName.setText("PINK");
-                        colorName.setTextColor(Color.parseColor("FFCOCB"));
+                        colorName.setTextColor(Color.rgb(255,20,147));
+                        star.setColorFilter(Color.rgb(255,20,147));
+                        btnPrev.setBackgroundColor(Color.rgb(255,20,147));
+                        btnNext.setBackgroundColor(Color.rgb(255,20,147));
                         break;
                     case "PINK":
                         colorName.setText("PURPLE");
-                        colorName.setTextColor(Color.parseColor("#6A0DAD"));
+                        colorName.setTextColor(Color.rgb(93,63,211));
+                        star.setColorFilter(Color.rgb(93,63,211));
+                        btnPrev.setBackgroundColor(Color.rgb(93,63,211));
+                        btnNext.setBackgroundColor(Color.rgb(93,63,211));
                         break;
                     case "PURPLE":
                         colorName.setText("YELLOW");
@@ -86,10 +100,10 @@ public class ColorActivity extends AppCompatActivity {
                         break;
                     case "BLUE":
                         colorName.setText("GREEN");
-                        colorName.setTextColor(Color.GREEN);
-                        star.setColorFilter(Color.GREEN);
-                        btnPrev.setBackgroundColor(Color.GREEN);
-                        btnNext.setBackgroundColor(Color.GREEN);
+                        colorName.setTextColor(Color.rgb(0,128,0));
+                        star.setColorFilter(Color.rgb(0,128,0));
+                        btnPrev.setBackgroundColor(Color.rgb(0,128,0));
+                        btnNext.setBackgroundColor(Color.rgb(0,128,0));
                         break;
                     case "GREEN":
                         colorName.setText("RED");
@@ -110,61 +124,82 @@ public class ColorActivity extends AppCompatActivity {
                 switch (color){
                     case "RED":
                         colorName.setText("GREEN");
-                        colorName.setTextColor(Color.GREEN);
-                        btnPrev.setBackgroundColor(Color.GREEN);
-                        btnNext.setBackgroundColor(Color.GREEN);
+                        colorName.setTextColor(Color.rgb(0,128,0));
+                        star.setColorFilter(Color.rgb(0,128,0));
+                        btnPrev.setBackgroundColor(Color.rgb(0,128,0));
+                        btnNext.setBackgroundColor(Color.rgb(0,128,0));
                         break;
                     case "GREEN":
                         colorName.setText("BLUE");
                         colorName.setTextColor(Color.BLUE);
                         btnPrev.setBackgroundColor(Color.BLUE);
                         btnNext.setBackgroundColor(Color.BLUE);
+                        star.setColorFilter(Color.BLUE);
                         break;
                     case "BLUE":
                         colorName.setText("YELLOW");
                         colorName.setTextColor(Color.YELLOW);
                         btnPrev.setBackgroundColor(Color.YELLOW);
                         btnNext.setBackgroundColor(Color.YELLOW);
+                        star.setColorFilter(Color.YELLOW);
                         break;
                     case "YELLOW":
                         colorName.setText("PURPLE");
-                        colorName.setTextColor(Color.parseColor("#6A0DAD"));
+                        colorName.setTextColor(Color.rgb(41,5,67));
+                        star.setColorFilter(Color.rgb(41,5,67));
+                        btnPrev.setBackgroundColor(Color.rgb(41,5,67));
+                        btnNext.setBackgroundColor(Color.rgb(41,5,67));
                         break;
                     case "PURPLE":
                         colorName.setText("PINK");
-                        colorName.setTextColor(Color.parseColor("FFCOCB"));
+                        colorName.setTextColor(Color.rgb(100,7,57));
+                        star.setColorFilter(Color.rgb(100,7,57));
+                        btnPrev.setBackgroundColor(Color.rgb(100,7,57));
+                        btnNext.setBackgroundColor(Color.rgb(100,7,57));
                         break;
                     case "PINK":
                         colorName.setText("ORANGE");
-                        colorName.setTextColor(Color.parseColor("FFA500"));
+                        colorName.setTextColor(Color.rgb(100,64,0));
+                        star.setColorFilter(Color.rgb(100,64,0));
+                        btnPrev.setBackgroundColor(Color.rgb(100,64,0));
+                        btnNext.setBackgroundColor(Color.rgb(100,64,0));
                         break;
                     case "ORANGE":
                         colorName.setText("BROWN");
-                        colorName.setTextColor(Color.parseColor("#964B00"));
+                        colorName.setTextColor(Color.rgb(165,42,42));
+                        star.setColorFilter(Color.rgb(165,42,42));
+                        btnPrev.setBackgroundColor(Color.rgb(165,42,42));
+                        btnNext.setBackgroundColor(Color.rgb(165,42,42));
                         break;
                     case "BROWN":
                         colorName.setText("GRAY");
                         colorName.setTextColor(Color.GRAY);
                         btnPrev.setBackgroundColor(Color.GRAY);
                         btnNext.setBackgroundColor(Color.GRAY);
+                        star.setColorFilter(Color.GRAY);
                         break;
                     case "GRAY":
                         colorName.setText("BLACK");
                         colorName.setTextColor(Color.BLACK);
-                        btnPrev.setBackgroundColor(Color.WHITE);
-                        btnNext.setBackgroundColor(Color.WHITE);
+                        btnPrev.setBackgroundColor(Color.BLACK);
+                        btnNext.setBackgroundColor(Color.BLACK);
+                        star.setColorFilter(Color.BLACK);
+                        btnPrev.setColorFilter(Color.WHITE);
+                        btnNext.setColorFilter(Color.WHITE);
                         break;
                     case "BLACK":
                         colorName.setText("WHITE");
                         colorName.setTextColor(Color.WHITE);
                         btnPrev.setBackgroundColor(Color.WHITE);
                         btnNext.setBackgroundColor(Color.WHITE);
+                        star.setColorFilter(Color.WHITE);
                         break;
                     case "WHITE":
                         colorName.setText("RED");
                         colorName.setTextColor(Color.RED);
                         btnPrev.setBackgroundColor(Color.RED);
                         btnNext.setBackgroundColor(Color.RED);
+                        star.setColorFilter(Color.RED);
                         break;
                 }
             }
