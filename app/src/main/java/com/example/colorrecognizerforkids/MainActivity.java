562db70332f84c0720bc74fafea23d838dbe9969
Mahreen-Asama <mahreenmehar202@gmail.com>
2022-05-19 21:55:07 +0500

    prev button added

--- app/src/main/java/com/example/colorrecognizerforkids/MainActivity.java
+++ app/src/main/java/com/example/colorrecognizerforkids/MainActivity.java
@@ -32,7 +32,55 @@ public class MainActivity extends AppCompatActivity {
         btnPrev.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                String color= (String) colorName.getText();
+                switch (color){
+                    case "RED":
+                        colorName.setText("WHITE");
+                        colorName.setTextColor(Color.WHITE);
+                        layout.setBackgroundColor(Color.LTGRAY);
+                        break;
+                    case "WHITE":
+                        colorName.setText("BLACK");
+                        colorName.setTextColor(Color.BLACK);
+                        break;
+                    case "BLACK":
+                        colorName.setText("GRAY");
+                        colorName.setTextColor(Color.GRAY);
+                        break;
+                    case "GRAY":
+                        colorName.setText("BROWN");
+                        colorName.setTextColor(Color.parseColor("#964B00"));
+                        break;
+                    case "BROWN":
+                        colorName.setText("ORANGE");
+                        colorName.setTextColor(Color.parseColor("FFA500"));
+                        break;
+                    case "ORANGE":
+                        colorName.setText("PINK");
+                        colorName.setTextColor(Color.parseColor("FFCOCB"));
+                        break;
+                    case "PINK":
+                        colorName.setText("PURPLE");
+                        colorName.setTextColor(Color.parseColor("#6A0DAD"));
+                        break;
+                    case "PURPLE":
+                        colorName.setText("YELLOW");
+                        colorName.setTextColor(Color.YELLOW);
+                        break;
+                    case "YELLOW":
+                        colorName.setText("BLUE");
+                        colorName.setTextColor(Color.BLUE);
+                        break;
+                    case "BLUE":
+                        colorName.setText("GREEN");
+                        colorName.setTextColor(Color.GREEN);
+                        break;
+                    case "GREEN":
+                        colorName.setText("RED");
+                        colorName.setTextColor(Color.RED);
+                        layout.setBackgroundColor(Color.WHITE);
+                        break;
+                }
             }
         });
 
