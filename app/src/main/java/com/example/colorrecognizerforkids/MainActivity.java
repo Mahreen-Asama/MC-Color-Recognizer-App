22bd60c150eba36aca16b8d4478cd7e20e662c34
Mahreen-Asama <mahreenmehar202@gmail.com>
2022-05-19 19:38:13 +0500

    colors added

--- app/src/main/java/com/example/colorrecognizerforkids/MainActivity.java
+++ app/src/main/java/com/example/colorrecognizerforkids/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.colorrecognizerforkids;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    ImageView color;
+    TextView colorName;
+    ImageButton btnPrev,btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        color=findViewById(R.id.color);
+        colorName=findViewById(R.id.name);
+
+        btnPrev=findViewById(R.id.btn_prev);
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
+        btnNext=findViewById(R.id.btn_next);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(colorName.equals("RED")){
+                    color.srcCompat="@drawable/ic_baseline_star_24";
+                }
+            }
+        });
     }
+
+    
 }
